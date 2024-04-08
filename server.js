93df9650abad415ade9cf2cf71f2a7c3aa0ae0d6
isaac <ipambrozevicius@gmail.com>
2024-04-07 21:54:31 -0300

    adicionar e ver os artigos - check

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const articlesFile = './articles.json';
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
@@ -17,8 +19,31 @@ app.get('/add', (req, res) => {
 });
 
 app.post('/add', (req, res) => {
+    const newArticle = {
+        title: req.body.title,
+        content: req.body.content,
+        category: req.body.category // Adicionando a categoria ao objeto do artigo
+    };
 
-    res.send('Artigo adicionado com sucesso!');
+    fs.readFile(articlesFile, (err, data) => {
+        if (err) {
+            console.error('Erro ao ler arquivo de artigos:', err);
+            res.status(500).send('Erro ao adicionar artigo.');
+            return;
+        }
+
+        let articles = JSON.parse(data);
+        articles.push(newArticle);
+
+        fs.writeFile(articlesFile, JSON.stringify(articles, null, 2), (err) => {
+            if (err) {
+                console.error('Erro ao escrever arquivo de artigos:', err);
+                res.status(500).send('Erro ao adicionar artigo.');
+                return;
+            }
+            res.send('Artigo adicionado com sucesso!');
+        });
+    });
 });
 
 app.get('/filter', (req, res) => {
@@ -26,7 +51,15 @@ app.get('/filter', (req, res) => {
 });
 
 app.get('/articles', (req, res) => {
-    res.sendFile(__dirname + '/articles.html');
+    fs.readFile(articlesFile, (err, data) => {
+        if (err) {
+            console.error('Erro ao ler arquivo de artigos:', err);
+            res.status(500).send('Erro ao buscar artigos.');
+            return;
+        }
+        const articles = JSON.parse(data);
+        res.send(articles);
+    });
 });
 
 app.listen(port, () => {
